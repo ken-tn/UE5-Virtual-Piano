0d658a3d39cc94063c9108531df4c65b4ab40696
kent <kent911t@gmail.com>
2025-02-09 11:18:37 +0000

    Update to Unreal Engine 5.5.2

--- Source/PianoEditor.Target.cs
+++ Source/PianoEditor.Target.cs
@@ -8,7 +8,8 @@ public class PianoEditorTarget : TargetRules
 	public PianoEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.AddRange( new string[] { "Piano" } );
 	}
 }
